ae0f2f4cfeef069aeda07f0d80876b6b20971ed1
Jose-17-11 <ariashdez@outlook.com>
2024-11-19 18:08:48 -0600

    add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,13 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
+  adapter: cloudflare({
+    imageService: 'cloudflare'
+ }),
+  output: 'hybrid',
 });
-
-// 
